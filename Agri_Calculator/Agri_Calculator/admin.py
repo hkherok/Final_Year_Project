4e385b68146cac49694bbf5b69b3ee905f80f38e
Azizul Haque Alif <55746705+azizulhaquealif@users.noreply.github.com>
2021-11-11 03:12:23 +0600

    project refreshig

--- Agri_Calculator/Agri_Calculator/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import problem, User_model
-
-admin.site.register(problem)
-admin.site.register(User_model)
---
+++ agri_calculator_backend/admin_main/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AdminMainConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'admin_main'
