3344cd08bd57b1e932263b3a341a2adc24e6985a
alpha artimos <remonansary@gmail.com>
2021-11-14 00:27:17 +0600

    nav

--- agri_calculator_frontend/src/App.js
+++ agri_calculator_frontend/src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./component/home/Home";
+import Navbar from "./component/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       propare manage
-      </header>
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/home" exact element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
