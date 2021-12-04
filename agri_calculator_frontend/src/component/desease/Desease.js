82eb81f4c34561c3dd0f65a393cb9b2b4be518d0
alpha artimos <remonansary@gmail.com>
2021-12-04 13:47:45 +0600

    disease

--- agri_calculator_frontend/src/component/desease/Desease.js
+++ agri_calculator_frontend/src/component/desease/Desease.js
@@ -1,170 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Navabar from "../Navbar/Navbar";
+
 import "./Desease.css";
 const Desease = () => {
+  const [APIData, setAPIData] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`https://615b12174a360f0017a81474.mockapi.io/fakedata`)
+      .then((response) => {
+        setAPIData(response.data);
+      });
+  }, []);
   return (
     <div>
       <div className="container">
         <div class="container bootstrap snipets">
-          <h1 class="text-center text-muted">Product catalog</h1>
+          <h1 class="text-center text-muted">Disease</h1>
           <div class="row flow-offset-2">
-            <div class="col-xs-6 col-md-4">
-              <div class="product tumbnail thumbnail-3">
-                <a href="#">
-                  <img
-                    src="https://via.placeholder.com/350x280/FFB6C1/000000"
-                    alt=""
-                  />
-                </a>
-                <div class="caption">
-                  <h6>
-                    <a href="#">Short Sleeve T-Shirt</a>
-                  </h6>
+            {APIData.map((data) => {
+              return (
+                <div class="col-xs-6 col-md-4">
+                  <div class="product tumbnail thumbnail-3">
+                    <a href="#">
+                      <img src={data.image} alt="" />
+                    </a>
+                    <div class="caption">
+                      <h6>
+                        <h3 href="#">{data.name}</h3>
+                        <a href="#">{data.reason}</a>
+                      </h6>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-            <div class="col-xs-6 col-md-4">
-              <div class="product tumbnail thumbnail-3">
-                <a href="#">
-                  <img
-                    src="https://via.placeholder.com/350x280/87CEFA/000000"
-                    alt=""
-                  />
-                </a>
-                <div class="caption">
-                  <h6>
-                    <a href="#">Short Sleeve T-Shirt</a>
-                  </h6>
-                </div>
-              </div>
-            </div>
-            <div class="col-xs-6 col-md-4">
-              <div class="product tumbnail thumbnail-3">
-                <a href="#">
-                  <img
-                    src="https://via.placeholder.com/350x280/FF7F50/000000"
-                    alt=""
-                  />
-                </a>
-                <div class="caption">
-                  <h6>
-                    <a href="#">Short Sleeve T-Shirt</a>
-                  </h6>
-                  <span class="price">$12.49</span>
-                </div>
-              </div>
-            </div>
-            <div class="col-xs-6 col-md-4">
-              <div class="product tumbnail thumbnail-3">
-                <a href="#">
-                  <img
-                    src="https://via.placeholder.com/350x280/20B2AA/000000"
-                    alt=""
-                  />
-                </a>
-                <div class="caption">
-                  <h6>
-                    <a href="#">Short Sleeve T-Shirt</a>
-                  </h6>
-                  <span class="price">
-                    <del>$24.99</del>
-                  </span>
-                  <span class="price sale">$12.49</span>
-                </div>
-              </div>
-            </div>
-            <div class="col-xs-6 col-md-4">
-              <div class="product tumbnail thumbnail-3">
-                <a href="#">
-                  <img
-                    src="https://via.placeholder.com/350x280/8A2BE2/000000"
-                    alt=""
-                  />
-                </a>
-                <div class="caption">
-                  <h6>
-                    <a href="#">Short Sleeve T-Shirt</a>
-                  </h6>
-                  <span class="price">
-                    <del>$24.99</del>
-                  </span>
-                  <span class="price sale">$12.49</span>
-                </div>
-              </div>
-            </div>
-            <div class="col-xs-6 col-md-4">
-              <div class="product tumbnail thumbnail-3">
-                <a href="#">
-                  <img
-                    src="https://via.placeholder.com/350x280/6495ED/000000"
-                    alt=""
-                  />
-                </a>
-                <div class="caption">
-                  <h6>
-                    <a href="#">Short Sleeve T-Shirt</a>
-                  </h6>
-                  <span class="price">
-                    <del>$24.99</del>
-                  </span>
-                  <span class="price sale">$12.49</span>
-                </div>
-              </div>
-            </div>
-            <div class="col-xs-6 col-md-4">
-              <div class="product tumbnail thumbnail-3">
-                <a href="#">
-                  <img
-                    src="https://via.placeholder.com/350x280/00008B/000000"
-                    alt=""
-                  />
-                </a>
-                <div class="caption">
-                  <h6>
-                    <a href="#">Short Sleeve T-Shirt</a>
-                  </h6>
-                  <span class="price">$12.49</span>
-                </div>
-              </div>
-            </div>
-            <div class="col-xs-6 col-md-4">
-              <div class="product tumbnail thumbnail-3">
-                <a href="#">
-                  <img
-                    src="https://via.placeholder.com/350x280/9932CC/000000"
-                    alt=""
-                  />
-                </a>
-                <div class="caption">
-                  <h6>
-                    <a href="#">Short Sleeve T-Shirt</a>
-                  </h6>
-                  <span class="price">
-                    <del>$24.99</del>
-                  </span>
-                  <span class="price sale">$12.49</span>
-                </div>
-              </div>
-            </div>
-            <div class="col-xs-6 col-md-4">
-              <div class="product tumbnail thumbnail-3">
-                <a href="#">
-                  <img
-                    src="https://via.placeholder.com/350x280/FF69B4/000000"
-                    alt=""
-                  />
-                </a>
-                <div class="caption">
-                  <h6>
-                    <a href="#">Short Sleeve T-Shirt</a>
-                  </h6>
-                  <span class="price">
-                    <del>$24.99</del>
-                  </span>
-                  <span class="price sale">$12.49</span>
-                </div>
-              </div>
-            </div>
+              );
+            })}
           </div>
         </div>
       </div>
