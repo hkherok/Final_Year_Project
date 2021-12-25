a2eaa3b11519ef3cf68f0ff25c55f6a566e1d45e
hkherok <herokk6@gmail.com>
2021-12-26 03:52:33 +0600

    letest update

--- agri_calculator_frontend/src/component/desease/Desease.js
+++ agri_calculator_frontend/src/component/desease/Desease.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Navabar from "../Navbar/Navbar";
-import "./Desease.css";
 
 import "./Desease.css";
 const Desease = () => {
@@ -13,7 +12,7 @@ const Desease = () => {
   }, []);
   return (
     <div>
-      {/* <div className="container">
+      <div className="container">
         <div class="container bootstrap snipets">
           <h1 class="text-center text-muted">Disease </h1>
           <div class="row flow-offset-2">
@@ -36,38 +35,7 @@ const Desease = () => {
             })}
           </div>
         </div>
-      </div> */}
-
-      <section>
-        <div class="container">
-          <div class="text-center mb-5">
-            <h5 class="text-primary h6">Our Blog</h5>
-            <h2 class="display-20 display-md-18 display-lg-16">
-              Most recent our blog
-            </h2>
-          </div>
-          <div class="row">
-            {APIData.map((data) => {
-              return (
-                <div class="col-lg-4 col-md-6 mb-2-6">
-                  <article class="card card-style2">
-                    <div class="card-img">
-                      <img class="rounded-top" src={data.photo} alt="..." />
-                    </div>
-                    <div class="card-body">
-                      <h3 class="h5">
-                        <a href="#!">{data.title}</a>
-                      </h3>
-                      <p class="display-30">{data.description}</p>
-                      <a href="#!" class="read-more"></a>
-                    </div>
-                  </article>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-      </section>
+      </div>
     </div>
   );
 };
